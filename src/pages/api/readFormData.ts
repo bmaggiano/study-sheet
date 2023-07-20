2d24e8ee42fd08cd4848f5cba404efa82f22a559
bmaggiano <brandon.maggiano@gmail.com>
2023-07-20 11:58:54 -0700

    changed add note to push to planetscale db instead

--- src/pages/api/readFormData.ts
+++ src/pages/api/readFormData.ts
@@ -1,17 +1,17 @@
 // pages/api/readFormData.js
 
-import { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     try {
-      // Read the file contents (formData.json) synchronously
-      const data = fs.readFileSync("formData.json", "utf8");
-      const formData = JSON.parse(data);
+      const notes = await prisma.notes.findMany()
 
       // Respond with the formData array
-      res.status(200).json(formData);
+      res.status(200).json(notes);
     } catch (err) {
       // If the file does not exist or there's an error reading it, respond with an empty array
       res.status(200).json([]);
--- src/pages/api/saveFormData.ts
+++ src/pages/api/saveFormData.ts
@@ -1,11 +1,11 @@
 // pages/api/saveFormData.js
-
+import { PrismaClient, Prisma } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
+const prisma = new PrismaClient();
 
 const PASSWORD = process.env.PASSWORD; // Replace this with your actual password or store it securely in environment variables
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { term, description, category, password } = req.body;
 
@@ -17,8 +17,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     // Read existing data from the file (if any)
     let existingData = [];
     try {
-      const data = fs.readFileSync("formData.json", "utf8");
-      existingData = JSON.parse(data);
+      await prisma.notes.create({
+        data: {
+          term,
+          description,
+          category,
+        }
+      })
     } catch (err) {
       // If the file does not exist or there's an error reading it, ignore and continue with an empty array
     }
@@ -28,12 +33,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       term,
       description,
       category,
-      timestamp: new Date().toISOString(), // Add a timestamp for reference if needed
     };
     existingData.push(newData);
 
     // Write the updated array back to the file with indentation (2 spaces in this case)
-    fs.writeFileSync("formData.json", JSON.stringify(existingData, null, 2), "utf8");
 
     res.status(200).json({ message: "Form data saved successfully" });
   } else {
